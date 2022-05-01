68d1e612537d43804964c27514a725b119933165
Victor Daniel Herrera Ayos <pasiondelbarza@gmail.com>
2022-05-01 12:48:09 -0500

    Creacion de recursos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RidersModule } from './riders/riders.module';
+import { UsersModule } from './users/users.module';
+import { CountriesModule } from './countries/countries.module';
+import { UsersAddressModule } from './users_address/users_address.module';
+import { DriversModule } from './drivers/drivers.module';
+import { LicenseTypesModule } from './license_types/license_types.module';
+import { DriverLicensesModule } from './driver_licenses/driver_licenses.module';
 
 @Module({
-  imports: [],
+  imports: [RidersModule, UsersModule, CountriesModule, UsersAddressModule, DriversModule, LicenseTypesModule, DriverLicensesModule],
   controllers: [AppController],
   providers: [AppService],
 })
